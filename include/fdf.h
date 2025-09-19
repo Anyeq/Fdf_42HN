aef6bee10eb238d9e5952f2bfe21063182b14bef
asando <alexando17@yahoo.com>
2025-09-19 14:09:24 +0200

    including all necessary library on the header

---
+++ include/fdf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/19 13:50:35 by asando            #+#    #+#             */
+/*   Updated: 2025/09/19 14:06:19 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <math.h>
+# include "libft.h"
+# include "get_next_line_bonus.h"
+
+#endif
