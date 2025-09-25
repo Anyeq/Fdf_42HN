7895a7bd43c7471a56a8afe970c4ed2419d55e97
asando <alexando17@yahoo.com>
2025-09-25 17:52:56 +0200

    adding function into header

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 13:50:35 by asando            #+#    #+#             */
-/*   Updated: 2025/09/25 14:16:33 by asando           ###   ########.fr       */
+/*   Updated: 2025/09/25 17:52:03 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,20 @@
 
 typedef struct	s_map_data
 {
+	int	row_size;
+	int	column_size;
 	int	**z_data;
 }	t_map_data;
 
-typedef	struct	s_map_data
-{
-	char **raw_data;
+//parsing function and utils
+void	deep_free(void *content);
+int		parse_file(int file_fd, t_map_data *file_map);
 
-}	t_raw_data;
+//fdf_utils
+void	clean_map(t_map_data *map, int n_deep);
+void	exit_malloc_failed(t_list **raw_data, void (*del)(void *));
+
+//error management function
+void	error_check(int n_arg, char *file_path, int *fd_file);
+
 #endif
