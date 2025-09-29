41a92e014347badb36d12ff9b1ff58cf43c3aebe
asando <alexando17@yahoo.com>
2025-09-29 21:06:38 +0200

    restructuring function and file

---
+++ src/graphic_src/graphic_execution_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   graphic_execution_bonus.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 21:01:13 by asando            #+#    #+#             */
+/*   Updated: 2025/09/29 21:03:11 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_point_project	ft_project(int x, int y, int z, t_camera cam)
+{
+	t_point_project	p;
+
+	p.fx = (x - y) * cos(cam.angle) * cam.zoom + cam.off_x;
+	p.fy = (x + y) * sin(cam.angle) * cam.zoom - (z * cam.elevation)
+		+ cam.off_y;
+	p.x = (int)fx;
+	p.y = (int)fy;
+	return (p);
+}
